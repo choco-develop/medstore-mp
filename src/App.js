5673cdbf3a0c9c8343f6700323cd0b2c556dc982
bushmusi <bushera.mestofa@gmail.com>
2022-07-05 15:58:28 +0300

    Add new component to index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Header from './components/header';
 import Slider from './components/slider';
+import Category from './components/category';
+import SampleProduct from './components/sample_product';
+import ClientComment from './components/client';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Slider />
+      <Category />
+      <SampleProduct />
+      <ClientComment />
+      <Footer />
     </div>
   );
 }
