14e348863b13cd9855f750e943926db5ee6b482c
bushmusi <bushera.mestofa@gmail.com>
2022-07-11 17:28:17 +0300

    Update tailwind css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
+    colors: {
+      main: '#35b8be'
+    },
     extend: {},
   },
   plugins: [
