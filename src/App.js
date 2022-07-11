596378d4d92f77819107cf4205fba6505e145503
bushmusi <bushera.mestofa@gmail.com>
2022-07-11 17:30:09 +0300

    comment components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import Header from './components/header';
-import Slider from './components/slider';
-import Category from './components/category';
-import SampleProduct from './components/sample_product';
-import ClientComment from './components/client';
-import Footer from './components/footer';
+// import Slider from './components/slider';
+// import Category from './components/category';
+// import SampleProduct from './components/sample_product';
+// import ClientComment from './components/client';
+// import Footer from './components/footer';
 
 function App() {
   return (
     <div>
       <Header />
-      <Slider />
+      {/* <Slider />
       <Category />
       <SampleProduct />
       <ClientComment />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
