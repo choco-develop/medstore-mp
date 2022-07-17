ad0680db64c2fb31eb5e019ba5a870aa9f283542
bushmusi <bushera.mestofa@gmail.com>
2022-07-17 18:07:25 +0300

    Update tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
     colors: {
       main: '#35b8be'
     },
+    screens: {
+      'xs': {'min': '329px', 'max': '640px'},
+      'sm': {'min': '640px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1535px'},
+      '2xl': {'min': '1536px'},
+    },
     extend: {},
   },
   plugins: [
