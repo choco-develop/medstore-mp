2287d71f208df8ecb026e181d51be95197ffda80
bushmusi <bushera.mestofa@gmail.com>
2022-07-18 17:26:07 +0300

    Add cat component

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,92 +1,29 @@
 import React from 'react';
-import { Carousel } from 'flowbite-react';
+import { Button } from 'flowbite-react';
 
 export default function category() {
+  const cats = [];
+  for (let i = 0; i < 24; i += 1) {
+    cats.push(
+      <div className="border flex flex-col">
+        <div>
+          <img src="./assets/cat.jpg" alt="catagory name" />
+        </div>
+        <div className="p-1">
+          <p className="font-sans text-sm font-light truncate text-center">Category 1</p>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
-      <div className="grid grid-cols-3 grid-rows-2 mt-5 p-2 border gap-1">
-        <div className="border flex flex-col">
-          <div>
-            <img src="./assets/cat.jpg" alt="catagory name" />
-          </div>
-          <div className="p-1">
-            <p className="font-sans text-sm font-light truncate text-center">Category 1</p>
-          </div>
-        </div>
-
-        <div className="border flex flex-col">
-          <div>
-            <img src="./assets/cat.jpg" alt="catagory name" />
-          </div>
-          <div className="p-1">
-            <p className="font-sans text-sm font-light truncate text-center">Category 2</p>
-          </div>
-        </div>
-
-        <div className="border flex flex-col">
-          <div>
-            <img src="./assets/cat.jpg" alt="catagory name" />
-          </div>
-          <div className="p-1">
-            <p className="font-sans text-sm font-light truncate text-center">Category 3</p>
-          </div>
-        </div>
-
-        <div className="border flex flex-col">
-          <div>
-            <img src="./assets/cat.jpg" alt="catagory name" />
-          </div>
-          <div className="p-1">
-            <p className="font-sans text-sm font-light truncate text-center">Category 4</p>
-          </div>
-        </div>
-
-        <div className="border flex flex-col">
-          <div>
-            <img src="./assets/cat.jpg" alt="catagory name" />
-          </div>
-          <div className="p-1">
-            <p className="font-sans text-sm font-light truncate text-center">Category 5</p>
-          </div>
-        </div>
-
-        <div className="border flex flex-col">
-          <div>
-            <img src="./assets/cat.jpg" alt="catagory name" />
-          </div>
-          <div className="p-1">
-            <p className="font-sans text-sm font-light truncate text-center">Category 6</p>
-          </div>
-        </div>
+      <div className="grid grid-cols-12 grid-rows-2 mt-10 mx-10 mb-2 p-5 border gap-5">
+        { cats }
       </div>
-
-      <div className="flex justify-center m-3">
-        <button type="button" className="outline-2 outline-blue-500/50">See all category</button>
-      </div>
-
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-        <Carousel>
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-            alt="..."
-          />
-          <img
-            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-            alt="..."
-          />
-        </Carousel>
+      <div className="flex justify-center mt-1">
+      <Button color="gray">
+        See All Category
+      </Button>
       </div>
     </>
   );
