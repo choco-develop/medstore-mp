016002aa83f573d312e4f2607788d63e1f5dae1d
bushmusi <bushera.mestofa@gmail.com>
2022-07-18 17:26:15 +0300

    Add slider

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Carousel } from 'flowbite-react';
 
 export default function slider() {
   return (
-    <div className="h-72 border flex justify-center items-center">
-      slider image here
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel slideInterval={1000}>
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+          alt="..."
+        />
+      </Carousel>
     </div>
   );
 }
