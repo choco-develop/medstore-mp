7855b4c8b1f78bd169eb959e8171be736b8ab21d
bushmusi <bushera.mestofa@gmail.com>
2022-07-25 16:37:34 +0300

    Add reponsive ness of the page

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 // import { Button } from 'flowbite-react';
 
 export default function category() {
-  const cats = [];
+  const catMax = [];
   for (let i = 0; i < 24; i += 1) {
-    cats.push(
+    catMax.push(
       <div className="border flex flex-col transition ease-in-out delay-250 hover:translate-y-1 hover:border-teal-400 hover:border-2" key={i}>
         <div>
           <img src="./assets/cat.jpg" alt="catagory name" />
@@ -17,10 +17,10 @@ export default function category() {
   }
   return (
     <>
-      <div className="grid grid-cols-10 grid-rows-2 mt-10 mx-10 mb-2 p-5 border gap-5 bg-stone-400 shadow-md">
-        { cats }
+      <div className="grid grid-cols-10 grid-rows-2 mt-10 mx-10 mb-2 p-5 border gap-5 bg-stone-400 shadow-md xs:hidden">
+        { catMax }
       </div>
-      <div className="flex justify-center mt-1">
+      <div className="flex justify-center mt-1 xs:hidden">
         <button type="button" className="px-5 py-3 my-5 border hover:rounded-md hover:bg-gray-200">
           See All Category
         </button>
