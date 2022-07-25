fd5830033938329154f5722dd3ed311b1a0ff489
bushmusi <bushera.mestofa@gmail.com>
2022-07-25 16:41:48 +0300

    Fix linter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     colors: {
-      main: '#35b8be'
+      main: '#35b8be',
     },
     screens: {
-      'xs': {'min': '329px', 'max': '640px'},
-      'sm': {'min': '640px', 'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px', 'max': '1535px'},
-      '2xl': {'min': '1536px'},
+      xs: { min: '329px', max: '640px' },
+      sm: { min: '640px', max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px', max: '1279px' },
+      xl: { min: '1280px', max: '1535px' },
+      '2xl': { min: '1536px' },
     },
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
+    require('flowbite/plugin'), {/* eslint-disable-line */}
   ],
 };
