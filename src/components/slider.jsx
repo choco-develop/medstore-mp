5c1ebed21b0a57014eb493fe15ea4bd943527e5f
bushmusi <bushera.mestofa@gmail.com>
2022-08-02 17:28:43 +0300

    Upate sliders

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 import { Carousel } from 'flowbite-react';
+import FirstSlide from '../assets/images/are-you-importer.jpg';
+import SecondSlide from '../assets/images/buy-medical-equipment.jpg';
 
 export default function slider() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel slideInterval={1000}>
         <img
-          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+          src={FirstSlide}
           alt="..."
+          className="h-full"
         />
         <img
-          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+          src={SecondSlide}
           alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-          alt="..."
+          className="h-full"
         />
       </Carousel>
     </div>
