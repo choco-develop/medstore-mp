c4cc763c153c3b10c3b0009b1c55f652f27c6b7e
bushmusi <bushera.mestofa@gmail.com>
2022-08-03 17:26:01 +0300

    Add category slider btn

--- src/components/category-matrix/category.jsx
+++ src/components/category-matrix/category.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-// import { Button } from 'flowbite-react';
+import { LeftSlide, RightSide } from './slider'; // eslint-disable-line
 
 export default function category() {
   const catMax = [];
-  for (let i = 0; i < 24; i += 1) {
+  for (let i = 0; i < 12; i += 1) {
     catMax.push(
-      <div className="border flex flex-col transition ease-in-out delay-250 hover:translate-y-1 hover:border-teal-400 hover:border-2" key={i}>
-        <div>
+      <div className="border flex flex-col transition ease-in-out delay-250 hover:translate-y-1 hover:border-teal-400 hover:border-2 z-0" key={i}>
+        <div className="relative">
+          <div className="p-1 absolute bottom-0 flex w-full justify-center">
+            <h3 className="text-[#364954]">Category Name</h3>
+          </div>
           <img src="./assets/cat.jpg" alt="catagory name" />
-        </div>
-        <div className="p-1">
-          <p className="font-sans text-sm font-light truncate text-center text-teal-900 leading-10 italic">{`category ${i}`}</p>
         </div>
       </div>,
     );
   }
+
   return (
     <>
-      <div className="grid grid-cols-10 grid-rows-2 mt-10 mx-10 mb-2 p-5 border gap-5 bg-stone-400 shadow-md xs:hidden">
+      <div className="grid grid-cols-6 grid-rows-2 mt-10 mx-10 mb-2 p-5 gap-5 bg-stone-400 xs:hidden bg-slate-200 relative">
+        <LeftSlide />
+        <RightSide />
         { catMax }
       </div>
       <div className="flex justify-center mt-1 xs:hidden">
---
+++ src/components/category-matrix/slider.js
@@ -0,0 +1,29 @@
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
+export function LeftSlide() {
+  return (
+    <div className="absolute top-0 left-0 flex flex-col
+                   h-full w-[150px]  bg-gradient-to-r
+                   from-[#E6F4F1] via-[#EDFCFF] to-transparent
+                   justify-center items-center z-50"
+    >
+      <button type="button">
+        <FaChevronLeft color="#1A8BB5" size={30} />
+      </button>
+    </div>
+  );
+}
+
+export function RightSide() {
+  return (
+    <div className="absolute top-0 right-0 flex flex-col
+                   h-full w-[150px]  bg-gradient-to-r
+                   from-transparent via-[#EDFCFF] to-[#E6F4F1]
+                   justify-center items-center z-50"
+    >
+      <button type="button">
+        <FaChevronRight color="#1A8BB5" size={30} />
+      </button>
+    </div>
+  );
+}
