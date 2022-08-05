8a55d2e5ed6e4678e564d5033acaee73ceecd4db
bushmusi <bushera.mestofa@gmail.com>
2022-08-05 17:18:36 +0300

    Add login form component

---
+++ src/components/auth/login-form.js
@@ -0,0 +1,9 @@
+export default function LoginForm() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex justify-center">
+        avator
+      </div>
+    </div>
+  );
+}
