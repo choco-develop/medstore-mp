d6880d45e2a7625ec85aea4bbbaa9ee8f5de1c9c
bushmusi <bushera.mestofa@gmail.com>
2022-08-07 17:06:04 +0300

    Update About UI

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,8 +4,8 @@ export default function AboutUs() {
   return (
     <div id="about-us" className="flex flex-col p-2 border mx-10 xs:mx-1 shadow-sm px-5 py-5 mb-5 bg-white">
       <div className="grid grid-cols-5 mt-5 gap-5">
-        <div className="col-span-1 xs:hidden">
-          <img src="./assets/logo-2.png" alt="company" className="h-64" />
+        <div className="col-span-1 xs:hidden grid grid-rows-3">
+          <img src="./assets/logo-2.png" alt="company" className="row-span-2  self-center" />
         </div>
         <div className="col-span-4 xs:col-span-5 ml-10">
           <p className="indent-10 text-justify text-gray-600">
