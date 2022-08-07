6f634bcb1cd66bd88275c0a7979e340990f083ba
bushmusi <bushera.mestofa@gmail.com>
2022-08-07 17:06:40 +0300

    Add more slider image

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Carousel } from 'flowbite-react';
 import FirstSlide from '../assets/images/are-you-importer.jpg';
 import SecondSlide from '../assets/images/buy-medical-equipment.jpg';
+import ThirdSlide from '../assets/images/marketplace-slider/diagnostic.jpg';
+import FourthSlide from '../assets/images/marketplace-slider/medical-expo.jpg';
+import FifthSlide from '../assets/images/marketplace-slider/qr-code.jpg';
 
 export default function slider() {
   return (
@@ -14,6 +17,21 @@ export default function slider() {
         />
         <img
           src={SecondSlide}
+          alt="..."
+          className="h-full"
+        />
+        <img
+          src={ThirdSlide}
+          alt="..."
+          className="h-full"
+        />
+        <img
+          src={FourthSlide}
+          alt="..."
+          className="h-full"
+        />
+        <img
+          src={FifthSlide}
           alt="..."
           className="h-full"
         />
