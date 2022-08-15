591c76afc087c2cf2334bce68e8f541ceedf70d3
bushmusi <bushera.mestofa@gmail.com>
2022-08-15 17:17:28 +0300

    Make visible some links

--- src/components/navigation/header.jsx
+++ src/components/navigation/header.jsx
@@ -1,29 +1,54 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { MdSearch, MdLogin } from 'react-icons/md';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import MainLogo from '../../assets/images/colored-logo.png';
+import MobileNav from './mobile-nav';
 
 export default function Header() {
   const [search, setSearch] = useState('');
+  const [navBar, setNavBar] = useState(false);
+
+  const handleMenuBtn = useCallback(() => {
+    setNavBar(!navBar);
+    console.log(navBar);
+  }, [navBar]);
+
   return (
-    <nav className="flex flex-col w-full">
+    <nav className="flex flex-col w-full relative">
       <div className="flex justify-between p-2 mini-nav content-center bg-top">
         <div>
-          <p className="customiseFont">Ethiopian Medical Material Portal</p>
+          <p className="font-serif">Ethiopian Medical Material Portal</p>
         </div>
-        <div className="flex items-center">
-          <a href="http://localhost:8000" className="customiseFont">Seller Portal</a>
+        <div className="flex items-center pr-2">
+          <a href="http://localhost:8000" className="font-serif">Seller Portal</a>
         </div>
       </div>
-      <div className="flex p-2 flex-wrap gap-y-2 bg-gradient-to-r from-main via-purple-500 to-temp">
+      <div className="flex p-2 flex-wrap gap-y-2 bg-main">
         <div className="flex gap-2">
-          <div style={{ width: '100px' }} className="relative">
-            <div style={{ height: '110px', width: '110px' }} className="w-full absolute top-0 left-0 z-50 bg-main rounded-full flex justify-center">
-              <div className="w-[90px] h-[90px] self-center bg-white rounded-full flex justify-center">
-                <img src={MainLogo} alt="..." className="w-[80px] self-center" />
-              </div>
+          <div
+            className="flex relative w-[55px]
+                    sm:w-[100px] md:w-[100px]
+                    lg:w-[100px] xl:w-[100px]
+                    2xl:w-[100px]"
+          >
+            <div
+              className="absolute top-0 left-0 z-50
+                        bg-main rounded-full flex
+                        self-center h-[55px] w-[55px]
+                        sm:w-[95px] sm:h-[95px] md:w-[95px] md:h-[95px]
+                        lg:w-[95px] lg:h-[95px] xl:w-[95px] xl:h-[95px]
+                        2xl:w-[95px] 2xl:h-[95px]
+                        "
+            >
+              <img
+                src={MainLogo}
+                alt="..."
+                className="object-scale-down
+                        bg-white rounded-full scale-90"
+              />
             </div>
           </div>
-          <h2 className="appName self-center">Medstore.et</h2>
+          <h2 className="text-white font-bold self-center font-serif">Medstore.et</h2>
         </div>
         <div className="flex-1 flex items-center justify-end">
           <ul className="flex w-full justify-end gap-x-4 px-3 items-center text-white xs:justify-end">
@@ -37,7 +62,7 @@ export default function Header() {
                   placeholder="Search here..."
                   value={search}
                   onChange={(e) => setSearch(e.target.value)}
-                  className="w-full py-3 pl-5 pr-4 border-none outline-none
+                  className="w-full py-3 pl-5 pr-4 outline-none
                               focus:border-transparent italic
                               placeholder:italic placeholder:text-white
                               bg-transparent border-none focus:ring-0"
@@ -47,22 +72,38 @@ export default function Header() {
                 </button>
               </div>
             </li>
-            <li className="xs:hidden navItems">
-              <a href="http://localhost:8000" className="hover:text-main">Buy Equipment</a>
+            <li className="xs:hidden sm:block navItems">
+              <a href="http://localhost:8000" className="hover:text-gray-200 font-serif">Buy Equipment</a>
             </li>
-            <li className="xs:hidden navItems">
-              <a href="http://localhost:8000" className="hover:text-main">News/Article</a>
+            <li className="xs:hidden sm:block navItems">
+              <a href="http://localhost:8000" className="hover:text-gray-200 font-serif">News/Article</a>
             </li>
-            <li className="xs:hidden navItems">
-              <a href="http://localhost:8000" className="hover:text-main">Tender</a>
+            <li className="xs:hidden sm:block navItems">
+              <a href="http://localhost:8000" className="hover:text-gray-200 font-serif">Tender</a>
             </li>
             <li className="navItems">
-              <a href="http://localhost:3000/login" className="flex items-center gap-1 hover:text-main">
+              <button
+                type="button"
+                className="flex items-center gap-1 hover:text-gray-200
+                          sm:hidden md:hidden lg:hidden xl:hidden 2xl:hidden"
+                onClick={() => handleMenuBtn()}
+              >
+                <GiHamburgerMenu size={25} />
+              </button>
+            </li>
+            <li className="xs:hidden sm:block navItems">
+              <a href="http://localhost:3000/login" className="flex items-center gap-1 hover:text-gray-200">
                 <MdLogin />
-                <p>Login</p>
+                <p className="font-serif">Login</p>
               </a>
             </li>
           </ul>
+          <section
+            className={`transition-all duration-500 ease-in absolute top-0 right-0 bg-main rounded-l-xl h-[100vh] ${navBar ? 'w-3/4' : 'w-0'}`}
+            style={{ zIndex: '51' }}
+          >
+            <MobileNav handleMenuBtn={handleMenuBtn} navBarValue={navBar} />
+          </section>
         </div>
       </div>
     </nav>
