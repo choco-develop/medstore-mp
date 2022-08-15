182aa34cc7eceffd7215dd4aa20a198010fbd338
bushmusi <bushera.mestofa@gmail.com>
2022-08-15 17:15:41 +0300

    Update UI styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,6 @@ module.exports = {
       temp: '#89E2FF',
     },
     screens: {
-      // xs: { min: '329px', max: '640px' },
-      // sm: { min: '640px', max: '767px' },
-      // md: { min: '768px', max: '1023px' },
-      // lg: { min: '1024px', max: '1279px' },
-      // xl: { min: '1280px', max: '1535px' },
-      // '2xl': { min: '1536px' },
       xs: '329px',
       sm: '640px',
       md: '768px',
