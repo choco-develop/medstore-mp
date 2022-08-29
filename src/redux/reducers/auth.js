932d98eb60dcd7a07938474186e5c6aa0d45fa95
bushmusi <bushera.mestofa@gmail.com>
2022-08-29 20:02:06 +0300

    Add acc activate logic

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -4,13 +4,25 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  ACTIVATION_SUCCESS,
+  ACTIVATION_FAILER,
 } from '../actions/type';
 
 const user = JSON.parse(localStorage.getItem('user'));
 
 const initialState = user
-  ? { isLoggedIn: true, user, message: null }
-  : { isLoggedIn: false, user: null, message: null };
+  ? {
+    isLoggedIn: true,
+    user,
+    message: null,
+    activationStatus: null,
+  }
+  : {
+    isLoggedIn: false,
+    user: null,
+    message: null,
+    activationStatus: null,
+  };
 
 export default function authReducer(state = initialState, actions) {
   const { type, payload } = actions;
@@ -20,12 +32,14 @@ export default function authReducer(state = initialState, actions) {
         ...state,
         isLoggedIn: false,
         message: { isError: false, data: payload },
+        activationStatus: null,
       };
     case REGISTER_FAIL:
       return {
         ...state,
         isLoggedIn: false,
         message: { isError: true, data: payload },
+        activationStatus: null,
       };
     case LOGIN_SUCCESS:
       return {
@@ -33,6 +47,23 @@ export default function authReducer(state = initialState, actions) {
         isLoggedIn: true,
         user: payload.user,
         message: payload,
+        activationStatus: null,
+      };
+    case ACTIVATION_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true,
+        user: null,
+        message: payload,
+        activationStatus: true,
+      };
+    case ACTIVATION_FAILER:
+      return {
+        ...state,
+        isLoggedIn: false,
+        user: null,
+        message: payload,
+        activationStatus: false,
       };
     case LOGIN_FAIL:
       return {
@@ -40,6 +71,7 @@ export default function authReducer(state = initialState, actions) {
         isLoggedIn: false,
         user: null,
         message: payload,
+        activationStatus: false,
       };
     case LOGOUT:
       return {
@@ -47,6 +79,7 @@ export default function authReducer(state = initialState, actions) {
         isLoggedIn: false,
         user: null,
         message: payload,
+        activationStatus: false,
       };
     default:
       return state;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -10,6 +10,20 @@ const register = (userData) => axios.post(`${API_URL}/accounts/user_signup/`, us
   .then((res) => Promise.resolve(res))
   .catch((err) => Promise.reject(err));
 
+const activateAccount = (data) => axios.post(`${API_URL}/accounts/activate/`, data,
+  {
+    headers: {
+      'content-type': 'multipart/form-data',
+    },
+  })
+  .then((res) => {
+    if (res.data.token) {
+      localStorage.setItem('user', JSON.stringify(res.data));
+    }
+    return Promise.resolve(res);
+  })
+  .catch((err) => Promise.reject(err));
+
 const login = (username, password) => axios.post(`${API_URL}/accounts/login/`, {
   username,
   password,
@@ -23,6 +37,9 @@ const login = (username, password) => axios.post(`${API_URL}/accounts/login/`, {
 
 const logout = () => {
   localStorage.removeItem('user');
+  localStorage.removeItem('userEmail');
 };
 
-export default { register, login, logout };
+export default {
+  register, login, logout, activateAccount,
+};
