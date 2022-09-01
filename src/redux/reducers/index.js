80379683721f54060cdd0a5765c0ee044f291167
bushmusi <bushera.mestofa@gmail.com>
2022-09-01 18:29:40 +0300

    Add user info state

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import auth from './auth';
 import message from './message';
+import userInfo from './user_info';
 
 export default combineReducers({
   auth,
   message,
+  userInfo,
 });
