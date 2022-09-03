023714f1c8682d194f948b911ba3713a104005c7
bushmusi <bushera.mestofa@gmail.com>
2022-09-03 17:01:44 +0300

    add ethio state list

---
+++ src/data/EthioState.js
@@ -0,0 +1,17 @@
+export default const EthioState = [
+  'Addis Ababa',
+  'Afar',
+  'Amhara',
+  'Benishangul-Gumuz',
+  'Dire Dawa ',
+  'Gambela',
+  'Harari',
+  'Oromia',
+  'Sidama',
+  'Somali',
+  'South West',
+  'SNNPR',
+  'Tigray',
+]
+
+// export default EthioState;
