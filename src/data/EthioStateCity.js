42befe01795e8739a4e90558f9a0bd7ff5cc224f
bushmusi <bushera.mestofa@gmail.com>
2022-09-04 20:04:17 +0300

    rename ethio state

---
+++ src/data/EthioStateCity.js
@@ -0,0 +1,29 @@
+export const EthioState = [
+  'Addis Ababa',
+  'Afar',
+  'Amhara',
+  'Benishangul-Gumuz',
+  'Dire Dawa ',
+  'Gambela',
+  'Harari',
+  'Oromia',
+  'Sidama',
+  'Somali',
+  'South West',
+  'SNNPR',
+  'Tigray',
+];
+
+export const AddisSubCity = [
+  'Addis Ketema',
+  'Akaky Kaliti',
+  'Arada',
+  'Bole',
+  'Gullele',
+  'Kirkos',
+  'Kolfe Keranio',
+  'Lideta',
+  'Nifas Silk-Lafto',
+  'Yeka',
+  'Lemi Kura',
+];
