98f824db0cfed245e079e35e63d1e92e0e0a2aa0
bushmusi <bushera.mestofa@gmail.com>
2022-09-06 17:29:12 +0300

    add comp info action

--- src/redux/actions/user_info.js
+++ src/redux/actions/user_info.js
@@ -1,6 +1,8 @@
 import {
   USER_INFO_REG_SUCCESS,
   USER_INFO_REG_FAILER,
+  COMPANY_INFO_REG_SUCCESS,
+  COMPANY_INFO_REG_FAILER,
 } from './type';
 import userService from '../../services/user-service';
 
@@ -40,5 +42,37 @@ const userInforDetail = (data) => (dispatch) => userService.userInfoReg(data).th
     return Promise.reject(err);
   },
 );
+
+export const companyInfo = (data) => (dispatch) => userService.companyInfoReg(data).then(
+  (res) => {
+    if (Object.hasOwn(res, 'data') && !res.data.err) {
+      dispatch({
+        type: COMPANY_INFO_REG_SUCCESS,
+        payload: res.data,
+      });
+    } else {
+      dispatch({
+        type: COMPANY_INFO_REG_FAILER,
+        payload: res.data,
+      })
+    }
+    return Promise.resolve(res);
+  },
+  (err) => {
+    if (err.response.status === 500) {
+      dispatch({
+        type: USER_INFO_REG_FAILER,
+        payload: { err: true, msg: 'Servor error: make sure you fill all fields!!!' },
+      });
+    } else {
+      dispatch({
+        type: USER_INFO_REG_FAILER,
+        payload: { err: true, msg: 'Unknow error occured' },
+      });
+    }
+    return Promise.reject(err);
+  },
+);
+
 
 export default userInforDetail;
