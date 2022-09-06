c8d022e98e19d39dd8fdeaa980c44db1a07dc0de
bushmusi <bushera.mestofa@gmail.com>
2022-09-06 17:29:21 +0300

    add comp reducers

--- src/redux/reducers/user_info.js
+++ src/redux/reducers/user_info.js
@@ -2,11 +2,16 @@ import {
   USER_INFO_REG_FAILER,
   USER_INFO_REG_SUCCESS,
   USER_INFO_REG_REQUEST,
+  COMPANY_INFO_REG_FAILER,
+  COMPANY_INFO_REG_SUCCESS,
+  COMPANY_INFO_REG_REQUEST,
 } from '../actions/type';
 
 const INIT_STATE = {
   message: null,
   loading: false,
+  companyMessage: null,
+  companyLoading: false,
 };
 
 export default function USER_INFO(state = INIT_STATE, action) {
@@ -17,18 +22,48 @@ export default function USER_INFO(state = INIT_STATE, action) {
         ...state,
         message: null,
         loading: true,
+        companyMessage: null,
+        companyLoading: false,
       };
     case USER_INFO_REG_SUCCESS:
       return {
         ...state,
         message: payload,
         loading: false,
+        companyMessage: null,
+        companyLoading: false,
       };
     case USER_INFO_REG_FAILER:
       return {
         ...state,
         message: payload,
         loading: false,
+        companyMessage: null,
+        companyLoading: false,
+      };
+    case COMPANY_INFO_REG_REQUEST:
+      return {
+        ...state,
+        message: null,
+        loading: false,
+        companyMessage: payload,
+        companyLoading: true,
+      };
+    case COMPANY_INFO_REG_SUCCESS:
+      return {
+        ...state,
+        message: null,
+        loading: false,
+        companyMessage: payload,
+        companyLoading: false,
+      };
+    case COMPANY_INFO_REG_FAILER:
+      return {
+        ...state,
+        message: null,
+        loading: false,
+        companyMessage: payload,
+        companyLoading: false,
       };
     default:
       return state;
