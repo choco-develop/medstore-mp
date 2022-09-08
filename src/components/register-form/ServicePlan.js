c51759c0dd2decae90daf0e3a6f03a492b0811a7
bushmusi <bushera.mestofa@gmail.com>
2022-09-08 19:04:20 +0300

    add service plan component

---
+++ src/components/register-form/ServicePlan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlanDesign from '../../assets/images/collection/service-plan.png';
+
+function ServicePlan() {
+  return (
+    <div className="flex bg-white w-full justify-center items-center">
+      <img src={PlanDesign} alt="..." className="w-[90%] h-[90%]" />
+    </div>
+  );
+}
+
+export default ServicePlan;
