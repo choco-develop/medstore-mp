14a6f82208c7219eefb17d75b59e074ae74ada71
bushmusi <bushera.mestofa@gmail.com>
2022-09-09 18:34:16 +0300

    ui update for news list in mobile version

--- src/components/news-article/news-index.js
+++ src/components/news-article/news-index.js
@@ -3,7 +3,7 @@ import NewsArticleComponent from './news-article';
 
 export default function NewsArticle() {
   return (
-    <div className="flex flex-col p-2 border xs:mx-2 my-5 shadow-sm px-5 py-5  bg-white">
+    <div className="flex flex-col p-2 border mx-0 sm:mx-2 my-5 shadow-sm px-5 py-5  bg-white">
       <div className="relative flex py-5 items-center">
         <div className="grow w-[10%] border" />
         <span
@@ -17,7 +17,7 @@ export default function NewsArticle() {
 
       <div
         className="flex flex-wrap
-                  xs:gap-y-3 xs:px-5
+                  xs:gap-y-3 xs:px-1
                   sm:px-0 sm:gap-y-0
                   md:gap-0
                   lg:gap-x-1 lg:justify-between
